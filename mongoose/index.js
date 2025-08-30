a6069e749f9ac805cca5a969f26b7d0e70a321cc
altr-r <arafatrahman0012@gmail.com>
2025-08-30 18:48:57 +0600

    created schema and a model from that schema, added data based on the model to the database

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,7 +1,26 @@
 const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const personScheam = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  age: Number,
+  bio: String,
+  single: Boolean,
+});
+
+const personModel = mongoose.model("Person", personScheam);
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/mongodemo")
-  .then(() => console.log("Database Connected"))
+  .then(async () => {
+    console.log("Database Connected");
+    const person = new personModel();
+    await person.save();
+    console.log("Person Created");
+    console.log(person);
+  })
   .catch((err) => console.log(err))
   .finally(() => mongoose.connection.close());
